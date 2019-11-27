5a93d108c0f67ad546aebafe4796a22401002940
Snow <snowjob46@yahoo.com>
2019-11-26 23:10:07 -0500

    Update stock_quote_scheduler.py

--- schedulerapp/main/scheduler/stock_quote_scheduler.py
+++ schedulerapp/main/scheduler/stock_quote_scheduler.py
@@ -14,6 +14,8 @@ parentdir = os.path.dirname(currentdir)
 parentdir = os.path.dirname(parentdir)
 parentdir = os.path.dirname(parentdir)
 
+sys.path.insert(0, parentdir)
+
 import schedule
 import time
 from schedulerapp.main.config import config_vars
@@ -64,11 +66,11 @@ if __name__ == "__main__":
     def request_batch(url=request_url, payload=request_payload):
         requests.post(url, json=payload)
 
-    schedule.every().monday.at("23:08").do(request_batch)
-    schedule.every().tuesday.at("23:08").do(request_batch)
-    schedule.every().wednesday.at("23:08").do(request_batch)
-    schedule.every().thursday.at("23:08").do(request_batch)
-    schedule.every().friday.at("23:08").do(request_batch)
+    schedule.every().monday.at("23:12").do(request_batch)
+    schedule.every().tuesday.at("23:12").do(request_batch)
+    schedule.every().wednesday.at("23:12").do(request_batch)
+    schedule.every().thursday.at("23:12").do(request_batch)
+    schedule.every().friday.at("23:12").do(request_batch)
 
     while True:
         schedule.run_pending()
