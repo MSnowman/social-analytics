4ee97990cd33bd9dc882f834f937d317549c203c
Snow <snowjob46@yahoo.com>
2019-12-14 22:40:40 -0500

    Update __init__.py

--- streamingapp/main/__init__.py
+++ streamingapp/main/__init__.py
@@ -1,18 +1,15 @@
 from flask import Flask, Blueprint
 from flask_restplus import Api
 from streamingapp.main.config import config_by_name
-from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 
 flask_bcrypt = Bcrypt()
-mongo = PyMongo()
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
     flask_bcrypt.init_app(app)
-    mongo.init_app(app)
     from streamingapp.main.router.streamer_routes import ns_streams
     api_blueprint = Blueprint('api', __name__)
     api = Api(api_blueprint,
